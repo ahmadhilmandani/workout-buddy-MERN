7e844352b3eb6b6b049f828d2fc0e40ce7334897
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2023-07-01 20:19:41 +0700

    UPDATE model of workout

--- backend/routes/workout.js
+++ backend/routes/workout.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const workoutModel = require('../models/m_workout')
 
 const routes = express.Router()
 
-// get all workouts
+// GET all workouts
 routes.get('/', (req, res) => {
   res.json({ msg: 'GET all workouts' })
 })
@@ -13,8 +14,16 @@ routes.get('/:id', (req, res) => {
 })
 
 // POST a a new workout
-routes.post('/', (req, res) => {
-  res.json({ msg: 'POST a new workout' })
+routes.post('/', async (req, res) => {
+  const { title, reps, load } = req.body
+  try {
+    const workout = await workoutModel.create({ title, reps, load })
+    res.status(200)
+    res.json(workout)
+  } catch (error) {
+    res.status(400)
+    res.json({ error: error.message })
+  }
 })
 
 // DELETE a a new workout
